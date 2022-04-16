bf81d0fa22b007e29c85c48f95a3dfa38696f8da
SandeepMourya <sandeepmourya0720@gmail.com>
2022-04-17 01:06:11 +0530

    fixed Upload Card scaling

--- src/components/UploadImage/UploadImage.js
+++ src/components/UploadImage/UploadImage.js
@@ -1,20 +1,21 @@
 import React from 'react';
-import { useState ,useEffect} from 'react';
+import { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import Navbar from '../UI/Navbar';
 import classes from './UploadImage.module.css'
-import { BarChart, Bar,XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
+import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
 
 import { Typography, Button, Input } from '@mui/material';
 
 
 function UploadImage() {
+    console.log("function Ran")
     const nav = useNavigate();
-    useEffect(()=>{
-        if(!localStorage.getItem('token')){
+    useEffect(() => {
+        if (!localStorage.getItem('token')) {
             nav('/Error')
-          }
-    },[nav])
+        }
+    }, [nav])
 
     console.log("dhdhdh")
     const [image, setImage] = useState("s");
@@ -24,41 +25,42 @@ function UploadImage() {
     const [fat, setfat] = useState(0);
     const [fibre, setfibre] = useState(0);
     const [weight, setWeight] = useState(500);
+    
 
     const data = [
         {
-          name: 'Caloies ⚡',
-          per_gram: calories/weight,
-         
-          
+            name: 'Caloies ⚡',
+            per_gram: calories / weight,
+
+
         },
         {
-          name: 'Protein 🥩',
-          per_gram: protein/weight,
-          
-          
+            name: 'Protein 🥩',
+            per_gram: protein / weight,
+
+
         },
         {
-          name: 'Carbs 🍚',
-          per_gram: carbohydrates/weight,
-         
-         
+            name: 'Carbs 🍚',
+            per_gram: carbohydrates / weight,
+
+
         },
         {
-          name: 'Fat 🧀',
-          per_gram: fat/weight,
-          
-         
+            name: 'Fat 🧀',
+            per_gram: fat / weight,
+
+
         },
         {
-          name: 'Fibre 🌿',
-          per_gram: fibre/weight,
-          
-          
+            name: 'Fibre 🌿',
+            per_gram: fibre / weight,
+
+
         },
-        
-        
-      ];
+
+
+    ];
 
     var formData = new FormData()
     formData.append('image', image)
@@ -84,18 +86,20 @@ function UploadImage() {
 
     }
 
-
+    
 
     const submit = async () => {
-
+       
         const response = await fetch("https://api.logmeal.es/v2/recognition/dish", { method: 'POST', body: formData, headers: myHeaders })
+        
         const result = await response.json();
-        console.log(response);
+        console.log(response.status);
 
         const response2 = await fetch(
             `https://api.edamam.com/api/nutrition-data?app_id=094741c7&app_key=a61fce81e1ac154b92edc42aa189029d&nutrition-type=logging&ingr=${result.recognition_results[0].name}`,
             requestOptions1
         )
+        
         const result2 = await response2.json()
         setcalories(result2.totalNutrients.ENERC_KCAL.quantity);
         setfat(result2.totalNutrients.FAT.quantity);
@@ -104,35 +108,35 @@ function UploadImage() {
         setfibre(result2.totalNutrients.FIBTG.quantity);
         setWeight(result2.totalWeight)
         console.log(result2);
-        
+
         const token = localStorage.getItem('token')
 
         var HeaderForfood = new Headers();
-        
+
         HeaderForfood.append("Authorization", `Bearer ${token}`);
         HeaderForfood.append("Content-Type", "application/json");
 
-        const data_to_send={
-            calories:result2.totalNutrients.ENERC_KCAL.quantity,
-            protein:result2.totalNutrients.PROCNT.quantity,
-            carbohydrates:result2.totalNutrients.CHOCDF.quantity,
-            fats:result2.totalNutrients.FAT.quantity,
-            fibres:result2.totalNutrients.FIBTG.quantity
+        const data_to_send = {
+            calories: result2.totalNutrients.ENERC_KCAL.quantity,
+            protein: result2.totalNutrients.PROCNT.quantity,
+            carbohydrates: result2.totalNutrients.CHOCDF.quantity,
+            fats: result2.totalNutrients.FAT.quantity,
+            fibres: result2.totalNutrients.FIBTG.quantity
         }
-        let raw=JSON.stringify(data_to_send)
+        let raw = JSON.stringify(data_to_send)
         console.log(raw)
         const responseFood = await fetch('https://myfitnesstracker-backend.herokuapp.com/foods', {
             method: 'POST',
             body: raw,
             headers: HeaderForfood,
-            
-          })
+
+        })
         const Data = await responseFood.json();
-        if(Data){
-            console.log(Data.createdAt.slice(0,10))
+        if (Data) {
+            console.log(Data.createdAt.slice(0, 10))
         }
         console.log(Data);
-       
+
 
     }
 
@@ -162,30 +166,33 @@ function UploadImage() {
                             >
                                 <Typography variant='body1'> Upload </Typography>
                             </Button>
-                            <div className={classes.Data}>
-                                Calories: {calories} kcal ⚡
+                            
+                                <div className={classes.Data}>
+                                    Calories: {calories} kcal ⚡
 
-                            </div>
-                            <div className={classes.Data}>
-                                Proteins: {protein} grams 🥩
-                            </div>
-                            <div className={classes.Data}>
-                                Carbohydrates: {carbohydrates} grams 🍚
-                            </div>
-                            <div className={classes.Data}>
-                                Fibres: {fibre} grams 🌿
-                            </div>
-                            <div className={classes.Data}>
-                                Fats: {fat} grams 🧀
-                            </div>
+                                </div>
+                                <div className={classes.Data}>
+                                    Proteins: {protein} grams 🥩
+                                </div>
+                                <div className={classes.Data}>
+                                    Carbohydrates: {carbohydrates} grams 🍚
+                                </div>
+                                <div className={classes.Data}>
+                                    Fibres: {fibre} grams 🌿
+                                </div>
+                                <div className={classes.Data}>
+                                    Fats: {fat} grams 🧀
+                                </div>
+                            
+
                         </div>
                     </div>
                     <div className={classes.chart}>
-                    <div className={classes.ChartTitle}>Nutritional Density</div>
+                        <div className={classes.ChartTitle}>Nutritional Density</div>
                         <ResponsiveContainer width="100%" height="90%">
-                        
+
                             <BarChart
-                                
+
                                 width={500}
                                 height={300}
                                 data={data}
@@ -198,10 +205,10 @@ function UploadImage() {
                             >
                                 <CartesianGrid strokeDasharray="3 3" />
                                 <XAxis dataKey="name" />
-                                <YAxis type="number" domain={[0, 3]}/>
+                                <YAxis type="number" domain={[0, 3]} />
                                 <Tooltip />
                                 <Legend />
-                                
+
                                 <Bar dataKey="per_gram" fill="#82ca9d" />
                             </BarChart>
                         </ResponsiveContainer>
---
+++ src/components/UploadImage/loader.js
@@ -0,0 +1,19 @@
+
+import classes from "./loader.module.css"
+
+
+const Loader = () => {
+
+
+
+    return (
+        <>
+            <div className={classes.wrapper}>
+                <div className={classes.loader}></div>
+
+            </div>
+
+        </>
+    )
+}
+export default Loader
